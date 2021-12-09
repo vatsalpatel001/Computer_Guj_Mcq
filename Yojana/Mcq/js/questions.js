92aeee2aa6c8aa8d216ee3f859c0cb20098867a2
vatsalpatel001 <vatsalpatel001@gmail.com>
2021-12-09 22:54:34 +0530

    Yojana Init

---
+++ Yojana/Mcq/js/questions.js
@@ -0,0 +1,18 @@
+// creating an array and passing the number, questions, options, and answers
+let questions_master = [
+    {    
+    question: "ધો-૧૨ વિજ્ઞાન પ્રવાહ પાસ કરી મેડીકલ એન્‍જીનીયરીંગ, પ્રવેશ પરીક્ષાની પૂર્વ તૈયારી માટે વિધાર્થીદીઠ કેટલા રૂપિયાની સહાય કરવામાં આવે છે?",
+    answer: "૨૦૦૦૦",
+    options: [
+      "૨૦૦૦",
+      "૧૦૦૦૦",
+      "૫૦૦૦",
+    ]
+  }
+];
+
+
+
+
+
+
