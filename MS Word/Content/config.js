9c6e1c6e68e202a2318c32dd713f1840bcc7f305
vatsal patel <vatsalpatel001@gmail.com>
2021-12-09 13:17:24 +0530

    init logic

---
+++ MS Word/Content/config.js
@@ -0,0 +1,14 @@
+let Heading =`Vatsal Patel`;
+
+let Slide_master_obj=[
+{
+	num:1,
+	nos:2,
+	title:"Default"
+},{
+	num:2,
+	nos:1,
+	title:"F8 Functionality"
+}
+
+]
---
+++ MS Word/Mcq/js/questions.js
@@ -0,0 +1,26 @@
+// creating an array and passing the number, questions, options, and answers
+let questions_master = [
+    {    
+    question: "માઇક્રોસોફ્ટ વર્ડ માં ફકરાનું સ્વરૂપ અને ઢાંચા ને જુદી જુદી કેટલી રીતે ગોઠવી શકાય છે.",
+    answer: "4",
+    options: [
+      "2",
+      "3",
+      "5",
+    ]
+  },{    
+    question: "માઇક્રોસોફ્ટ વર્ડમાં ફોન્ટની ડિફોલ્ટ સાઈઝ કેટલી હોય છે.",
+    answer: "11",
+    options: [
+      "8",
+      "12",
+      "14",
+    ]
+  },
+];
+
+
+
+
+
+
