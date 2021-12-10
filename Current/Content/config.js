2ccf15e13c1d2decc6a5d33fec443a004149f988
vatsalpatel001 <vatsalpatel001@gmail.com>
2021-12-10 20:39:02 +0530

    Current Init

---
+++ Current/Content/config.js
@@ -0,0 +1,14 @@
+let Heading =`Vatsal Patel`;
+
+let Slide_master_obj=[
+{
+	num:1,
+	nos:1,
+	title:"સશ્રી ઝુલન ગૌસ્વામી 600 વિકેટ"
+},{
+	num:2,
+	nos:1,
+	title:"ગુજરાત વિધાનસભાના પ્રથમ મહિલા અઘ્યક્ષઃ ડો. નીમાબેન આચાર્ય"
+},
+
+]
