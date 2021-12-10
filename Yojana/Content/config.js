00941940b7ce23c099623a866b18993e7fbb3550
vatsalpatel001 <vatsalpatel001@gmail.com>
2021-12-10 20:01:18 +0530

    new yojana added

--- Yojana/Content/config.js
+++ Yojana/Content/config.js
@@ -3,8 +3,12 @@ let Heading =`Vatsal Patel`;
 let Slide_master_obj=[
 {
 	num:1,
-	nos:1,
-	title:"Default"
-}
+	nos:3,
+	title:"વિધાર્થી યોજ્ના"
+},{
+	num:2,
+	nos:3,
+	title:"વિધાર્થી લોન યોજ્ના"
+},
 
 ]
